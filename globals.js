11ede2bba70ab8247a36bb082c5ddd354e1ebd18
Robin <robinhallerbach13@googlemail.com>
2021-03-17 16:11:21 +0100

    init

---
+++ globals.js
@@ -0,0 +1,17 @@
+module.exports = {
+    WIN_HEIGHT: 500,
+    WIN_WIDTH: 520,
+    BLOCK_SIZE: 150,
+    BULB_IP: '192.168.2.108',
+    COLOR_MAP: {
+        RED: '255,0,0',
+        BLUE: '0,0,255',
+        GREEN: '0,255,0',
+        PURPLE: '255,0,255',
+        WHITE: '255,255,255',
+        WARM_WHITE: '255,233,155',
+        BLACK: '0,0,0',
+        YELLOW: '255,255,0',
+        CYAN: '0,255,255'
+    }
+}
--- renderer.js
+++ renderer.js
@@ -1,6 +1,77 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const {ipcRenderer} = require('electron')
+const globals = require('./globals')
+const BulbController = require("magic-hue-controller");
+ 
+const bulbController = new BulbController(globals.BULB_IP);
+
+
+window.addEventListener('keyup',(e)=>{
+    if(e.key === 'Escape') ipcRenderer.sendSync('close')
+})
+
+
+for(let color in globals.COLOR_MAP){
+    let colorElement = document.createElement('div')
+    let colorBar = document.createElement('div')
+    colorElement.className = 'colorElement'
+    colorElement.setAttribute('brightness','10')
+    colorElement.style.backgroundColor = `rgb(${globals.COLOR_MAP[color]
+        .split(',').map(c=>c-200).join(',')})`
+    colorElement.style.width = `${globals.BLOCK_SIZE}px`
+    colorElement.style.height = `${globals.BLOCK_SIZE}px`
+    colorElement.style.margin = `5px`
+    colorElement.style.webkitAppRegion = 'no-drag'
+    colorElement.onclick = () => {
+        if(color == 'WARM_WHITE'){
+            setWarm(colorElement.getAttribute('brightness'))
+        }else{
+            colorChange(globals.COLOR_MAP[color],colorElement.getAttribute('brightness'))
+        }
+    }
+    colorElement.onwheel = (e) => {
+        let multiplier = ((e.deltaY/100) * -1) 
+        if(colorElement.getAttribute('brightness')>=0 && colorElement.getAttribute('brightness')<=10){
+            colorElement.setAttribute('brightness',
+                parseInt(colorElement.getAttribute('brightness'))+multiplier)
+            if(colorElement.getAttribute('brightness')>=11) colorElement.setAttribute('brightness',10)
+            if(colorElement.getAttribute('brightness')<=-1) colorElement.setAttribute('brightness',0)
+        }
+        colorBar.style.height = `${(colorElement.getAttribute('brightness')/10)*100}%`
+        if(color == 'WARM_WHITE'){
+            setWarm(colorElement.getAttribute('brightness'))
+        }else{
+            colorChange(globals.COLOR_MAP[color],colorElement.getAttribute('brightness'))
+        }
+    }
+
+    colorBar.className = 'colorBar'
+    colorBar.style.backgroundColor = `rgb(${globals.COLOR_MAP[color]})`
+    colorBar.style.width = '100%'
+    colorBar.style.height = '100%'
+    colorElement.appendChild(colorBar)
+    document.body.appendChild(colorElement)
+}
+
+function colorChange(color,brightness){
+    bulbController.isOnline().then(async (status) => {
+        if(status) {
+            await bulbController.sendPower(true)
+            await bulbController.sendRGB(color.split(',').map(c=>{
+                return c - (((10-brightness)/10)*255) < 0 ? 0 :c - (((10-brightness)/10)*255)
+            }).join(','))
+        }else {
+            console.log("offline")
+        }
+    })
+}
+
+function setWarm(brightness){
+    bulbController.isOnline().then(async (status) => {
+        if(status) {
+            await bulbController.sendPower(true)
+            await bulbController.sendWarmLevel(255*brightness/10)
+        }else {
+            console.log("offline")
+        }
+    })
+}
